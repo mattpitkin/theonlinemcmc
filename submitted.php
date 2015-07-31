76ac19383f40cf5cf92c9f67dc5f1b6247d5fbce
Matthew Pitkin <matthew.pitkin@ligo.org>
2015-07-31 17:13:28 +0100

    submitted.php: minor change that allows the code to run in the correct directory

--- submitted.php
+++ submitted.php
@@ -14,9 +14,9 @@ session_start();
 $outdir = $_SESSION["outdir"];
 
 // submit a process to run the python script in the directory that has been passed through
-$errfile = $outdir.'/err_code.txt';
-$pycommand = $outdir.'/pyfile.py';
-$pid = shell_exec(sprintf('%s > %s 2>&1 & echo $!', $pycommand, $errfile));
+$errfile = 'err_code.txt';
+$pycommand = './pyfile.py';
+$pid = shell_exec(sprintf('cd %s; %s > %s 2>&1 & echo $!', $outdir, $pycommand, $errfile));
 ?>
 
 <h1>Submission successful</h1>
