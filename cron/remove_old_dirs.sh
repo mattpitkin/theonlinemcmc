6099db7facf808f8553b734c84101ab6065b1bf8
Matthew Pitkin <matthew.pitkin@ligo.org>
2015-08-07 16:51:40 +0100

    Added bash script to run with cron for removing old directories

---
+++ cron/remove_old_dirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# a script to run with cron daily (place in /etc/cron/daily) to remove
+# user created directories after 15 days
+
+# get environment variable with results directory location
+RESDIR=$ONLINEMCMC_RESULTS_DIR
+
+# set 15 day limit on directory modificate (which should be the creation time)
+modlim=15
+
+# use find to find and delete the directories and their content
+find ${RESDIR}/* -type d -mtime ${modlim} -exec rm -rf {} +
