61f91f30c2ca2d69e7a512d0c4382766ac22bca5
Matt Pitkin <pitkin@gmail.com>
2015-08-23 22:03:14 +0100

    Removed attempts at PHP filter sanitization of POST variables
    closes #4

--- index.php
+++ index.php
@@ -28,16 +28,6 @@
 <!-- php code to write out python and submit process -->
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // sanitize the inputs that were not directly created by the javascript code (see e.g. http://uk1.php.net/manual/en/filter.filters.sanitize.php)
-  //$postfilter = filter_input_array(
-  //  INPUT_POST,
-  //  array(
-  //    'abscissa_data' => array('filter' => FILTER_VALIDATE_FLOAT),
-  //    'input_data'    => array('filter' => FILTER_VALIDATE_FLOAT),
-  //    'sigma_data'    => array('filter' => FILTER_VALIDATE_FLOAT)
-  //  )
-  //);
-  
   $resdir = 'results';
   
   if (!empty($_POST["pyfile"])) {
@@ -85,9 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!file_exists($outdir)){
       mkdir($outdir, 0777, true);
     }
-    //$absdata = filter_var($_POST["abscissa_data"], FILTER_VALIDATE_FLOAT);
-    //file_put_contents($outdir.'/abscissa_file.txt', $absdata);
-    //file_put_contents($outdir.'/abscissa_file.txt', $postfilter["abscissa_data"]);
+
     file_put_contents($outdir.'/abscissa_file.txt', $_POST["abscissa_data"]);
   }
 
@@ -97,9 +85,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!file_exists($outdir)){
       mkdir($outdir, 0777, true);
     }
-    //$indata = filter_var($_POST["input_data"], FILTER_VALIDATE_FLOAT);
-    //file_put_contents($outdir.'/data_file.txt', $indata);
-    //file_put_contents($outdir.'/data_file.txt', $postfilter["input_data"]);
+
     file_put_contents($outdir.'/data_file.txt', $_POST["input_data"]);
   }
 
@@ -124,9 +110,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!file_exists($outdir)){
       mkdir($outdir, 0777, true);
     }
-    //$sidata = filter_var($_POST["sigma_data"], FILTER_VALIDATE_FLOAT);
-    //file_put_contents($outdir.'/sigma_file.txt', $sidata);
-    //file_put_contents($outdir.'/sigma_file.txt', $postfilter["sigma_data"]);
+
     file_put_contents($outdir.'/sigma_file.txt', $_POST["sigma_data"]);
   }
 
