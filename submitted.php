daa7ed601abb0411fffc0ddad5f3d50c8c332096
Cat <fanatapan@Cats-Mint>
2018-01-19 12:36:57 +0000

    add all changes for new layout

--- submitted.php
+++ submitted.php
@@ -1,27 +1,60 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>The Online MCMC: Submitted</title>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+<meta name="author" content="Matthew Pitkin">
+<meta name="description" content="The Online MCMC">
+<meta name="keywords" content="MCMC, Markov chain Monte Carlo, Bayesian, emcee, python, data analysis, probability">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- Include theme font -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+<!-- Include jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- custom CSS file -->
 <link rel="stylesheet" type="text/css" href="simple.css"/>
+
+<title>The Online MCMC: Submitted</title>
 </head>
+
 <body>
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="/">THE ONLINE MCMC</a>
+    </div>
+  </div>
+</nav>
 
-<div id="page-wrap">
+<div class="container-top bg-1 text-center">
+  <h3 class="title">SUBMISSION SUCCESSFUL</h3>
+  <p>
+    <br><br>Your submission has been successful. You should receive an email when the results have been completed.<br><br>
+  </p>
+  <br>
+</div>
 
-<!-- include header file -->
-<?php include('header.inc'); ?>
+<footer class="container-fluid bg-2 text-center">
+  <p class="footer"> <strong> &copy; Matthew Pitkin (2015). </strong>The code for this site is licensed under the <a style="color: #BD5D38" href="http://opensource.org/licenses/MIT">MIT license</a>. It is available on <a style="color: #BD5D38" href="https://github.com/mattpitkin/theonlinemcmc">github</a> and <a style="color: #BD5D38" href="https://bitbucket.org/mattpitkin/theonlinemcmc">bitbucket</a>.<br>This site is kindly hosted by the <a style="color: #BD5D38" href="http://www.gla.ac.uk/schools/physics/">School of Physics & Astronomy</a> at the <a style="color: #BD5D38" href="http://www.gla.ac.uk/">University of Glasgow</a>. They bear no reponsibility for the content of this site or the results that are produced.
+  </p>
 
-<h2>Submission successful</h2>
-
-<p>
-Your submission has been successful. You should receive an email when the results have been completed.
-</p>
-
-<br>
-
-<!-- include footer file -->
-<?php include('footer.inc'); ?>
-
+<!-- include Social Media sharing file -->
+<?php
+$shareurl = "http://www.theonlinemcmc.com";
+include('social.inc');
+?>
 </div>
 </body>
 </html>
