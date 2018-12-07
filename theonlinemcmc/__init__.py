8775fa94eaff3bb017553690ee0a2119300e788f
Franciswebb <franciswebb7@gmail.com>
2018-12-07 10:01:57 +0000

    removing unnceecssary files

--- theonlinemcmc/__init__.py
+++ theonlinemcmc/__init__.py
@@ -1,5 +1,7 @@
-from emailresponse import *
-from postprocessing import *
-from errorcodes import *
-from errorpage import *
-from database import *
+from __future__ import absolute_import
+
+from .emailresponse import *
+from .postprocessing import *
+from .errorcodes import *
+from .errorpage import *
+from .database import *
