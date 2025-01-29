e65756e70e6d0857bcaa7b9470c974c6e472671a
Matt Pitkin <m@ttpitk.in>
2025-01-29 17:05:37 +0000

    Update submitted.php
     - remove bitbucket link

--- submitted.php
+++ submitted.php
@@ -47,7 +47,7 @@
 </div>
 
 <footer class="container-fluid bg-2 text-center">
-  <p class="footer"> <strong> &copy; Matthew Pitkin (2015). </strong>The code for this site is licensed under the <a style="color: #BD5D38" href="http://opensource.org/licenses/MIT">MIT license</a>. It is available on <a style="color: #BD5D38" href="https://github.com/mattpitkin/theonlinemcmc">github</a> and <a style="color: #BD5D38" href="https://bitbucket.org/mattpitkin/theonlinemcmc">bitbucket</a>.
+  <p class="footer"> <strong> &copy; Matthew Pitkin (2015). </strong>The code for this site is licensed under the <a style="color: #BD5D38" href="http://opensource.org/licenses/MIT">MIT license</a>. It is available on <a style="color: #BD5D38" href="https://github.com/mattpitkin/theonlinemcmc">github</a>.
   </p>
 
 <!-- include Social Media sharing file -->
